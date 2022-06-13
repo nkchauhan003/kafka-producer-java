c1ae9beceb5dcedcb1f20707e5f1349978a03310
nkchauhan003 <nkchauhan003@gmail.com>
2022-06-13 14:34:36 +0530

    First commit

--- src/main/java/com/cb/producer/DemoProducer.java
+++ src/main/java/com/cb/producer/DemoProducer.java
@@ -2,7 +2,8 @@ package com.cb.producer;
 
 import java.util.concurrent.ExecutionException;
 
-public interface DemoProducer<K,V> {
+public interface DemoProducer<K, V> {
     void produce(K key, V value) throws ExecutionException, InterruptedException;
+
     void produceAsync(K key, V value);
 }
